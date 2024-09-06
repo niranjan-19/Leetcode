1474cb7fb6f51f815bba44298de8097a06eab25d
Niranjan <130972659+niranjan-19@users.noreply.github.com>
2024-09-06 17:31:11 +0530

    Time: 1 ms (99.82%) | Memory: 46.2 MB (36.04%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,30 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-if (nums == null || nums.length == 0) {
-            throw new IllegalArgumentException("Array must not be null or empty");
-        }
-
-        int n = nums.length;
-        int min = nums[0];
-        int max = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[i]);
-        }
-
-        int range = max - min + 1;
-        int[] hash = new int[range];
-
-        for (int i = 0; i < n; i++) {
-            hash[nums[i] - min]++;
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (hash[nums[i] - min] == 1) {
-                return nums[i];
-            }
-        }
-        return -1 ;
+int n = nums.length ;
+int xor = 0 ;
+for(int i =0 ; i<n ;i ++){
+    xor = xor ^ nums[i];
+}
+return xor ;
     }
 }
