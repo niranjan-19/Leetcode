95255fbc72c06d9f0e662fc1c2b4efa5ce6c8ad6
Niranjan <130972659+niranjan-19@users.noreply.github.com>
2024-10-17 17:48:22 +0530

    Time: 17 ms (73.63%) | Memory: 43.8 MB (72.36%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,31 +1,45 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
-        Set<List<Integer>> st = new HashSet<>();
-
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             for (int j = i + 1; j < n; j++) {
-                Set<Long> hashset = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
 
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
                     long sum = nums[i] + nums[j];
                     sum += nums[k];
-                    long fourth = target - sum;
-                    if (hashset.contains(fourth)) {
+                    sum += nums[l];
+
+                    if (sum == target) {
                         List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
                         temp.add(nums[k]);
-                        temp.add((int) fourth);
-                        temp.sort(Integer::compareTo);
-                        st.add(temp);
-                    }
-                    
-                    hashset.add((long) nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+
+                    } else if (sum < target)
+                        k++;
+                    else
+                        l--;
+
                 }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
     }
 }
