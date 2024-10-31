ed032cb99e7ba4cc975cc940520c2db584044918
Niranjan <130972659+niranjan-19@users.noreply.github.com>
2024-10-31 07:24:12 +0530

    Time: 757 ms (47.28%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,6 +1,2 @@
-# Write your MySQL query statement below
-Select s.user_id , ROUND(avg(if(c.action="confirmed",1,0)), 2) AS  confirmation_rate
-FROM Signups AS s LEFT JOIN Confirmations AS c 
-ON 
-s.user_id = c.user_id 
-Group BY user_id ;
+select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
