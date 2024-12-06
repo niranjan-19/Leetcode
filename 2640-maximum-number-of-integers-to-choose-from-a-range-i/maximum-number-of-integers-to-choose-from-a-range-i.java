68baa18430f17a3326742e713794f7e195514015
Niranjan <130972659+niranjan-19@users.noreply.github.com>
2024-12-06 16:20:36 +0530

    Time: 4 ms (100.00%) | Memory: 45.9 MB (33.18%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,30 +1,20 @@
 class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
-        Arrays.sort(banned);
-        int total = 0, count = 0;
+        int sum = 0;
+        int result = 0;
+        boolean[] isBanned = new boolean[10001];
+        for (int ban : banned) {
+            isBanned[ban] = true;
+        }
         for (int i = 1; i <= n; i++) {
-            if (!binarySearch(banned, i)) {
-                total += i;
-                if (total <= maxSum)
-                    count++;
-                else
-                    break;
+            if (sum + i > maxSum) {
+                break;
+            }
+            if (!isBanned[i]) {
+                sum += i;
+                result++;
             }
         }
-        return count;
-    }
-
-    private boolean binarySearch(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == target)
-                return true;
-            if (arr[mid] < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-        return false;
+        return result;
     }
 }
